0168062e10db41f29ca68d7a5307531e05155950
Miguel Remédios <miguelremediios@gmail.com>
2024-04-17 00:00:39 +0100

    feat: utils mockdata and ES module type

--- server/index.js
+++ server/index.js
@@ -1,9 +1,35 @@
-const express = require('express');
-require('dotenv').config();
+import express from 'express';
+import { createHandler } from 'graphql-http';
+import dotenv from 'dotenv';
+
+import { ruruHTML } from 'ruru/server';
+
+import schema from './schema/schema.js';
+
+dotenv.config();
 
 const port = process.env.PORT || 3000;
 
 const app = express();
+
+var root = {
+  hello() {
+    return 'Hello world!';
+  },
+};
+
+app.all(
+  '/graphql',
+  createHandler({
+    schema: schema,
+    rootValue: root,
+  })
+);
+
+app.get('/', (_req, res) => {
+  res.type('html');
+  res.end(ruruHTML({ endpoint: '/graphql' }));
+});
 
 app.listen(
   port,
